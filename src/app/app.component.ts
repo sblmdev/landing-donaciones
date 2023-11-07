3360f8b890733b0026144bf402d894ea9b984db1
Bruno Andre Chavez Peralta <bchavezp@benelima.pe>
2023-11-07 17:38:39 -0500

    Avance Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'landing-donaciones';
+  item = 1;
+  activateTab(tabId: number) {
+    console.log(tabId);
+    this.item = tabId;
+    console.log(this.item)
+  }
+  bajar():void{
+    const documentHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const scrollDistance = documentHeight - windowHeight;
+    window.scrollTo({
+      top: scrollDistance,
+      behavior: 'smooth', 
+    });
+  }
 }
