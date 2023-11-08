1b35c9d3e145e6e4b3bbaf2c84b95bd3a7280f08
Bruno Andre Chavez Peralta <bchavezp@benelima.pe>
2023-11-08 18:02:01 -0500

    Segundo avance pasarela de pagos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
@@ -6,9 +6,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pantallaCargada = false;
+  
+ngOnInit(){
+  console.log(this.pantallaCargada)
+}
+
+
+  mostrar():void{
+    this.pantallaCargada = true
+    console.log("mostrando " + this.pantallaCargada)
+  }
   title = 'landing-donaciones';
-  item = 1;
-  activateTab(tabId: number) {
+  item = 2;
+  activateTab(tabId: number) :void{
+    //if(tabId==1)return;
     console.log(tabId);
     this.item = tabId;
     console.log(this.item)
